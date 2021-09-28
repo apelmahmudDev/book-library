31bcc4917100344aadcc29e5db7708e3bd58e589
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-09-28 19:02:13 +0600

    Dashboard taken from _app.js

--- contexts/DashboardContext.js
+++ contexts/DashboardContext.js
@@ -6,65 +6,65 @@ import usersData from "../data/usersData/usersData";
 const DashboardContext = createContext();
 
 const DashboardProvider = ({ children }) => {
-  const [section, setSection] = useState("addNewBook");
-  const sidebarRef = useRef(null);
-  const newBookRef = useRef(null);
-  const [newBookInfo, setNewBookInfo] = useState({
-    bookName: "",
-    authorName: "",
-    bookImg: "",
-    bookCategory: "",
-    bookPrice: "",
-  });
+	const [section, setSection] = useState("addNewBook");
+	const sidebarRef = useRef(null);
+	const newBookRef = useRef(null);
+	const [newBookInfo, setNewBookInfo] = useState({
+		bookName: "",
+		authorName: "",
+		bookImg: "",
+		bookCategory: "",
+		bookPrice: "",
+	});
 
-  const showSidebar = () => {
-    sidebarRef.current.classList.toggle("-translate-x-full");
-  };
+	const showSidebar = () => {
+		sidebarRef.current.classList.toggle("-translate-x-full");
+	};
 
-  const newBookInfoHandler = (e) => {
-    e.preventDefault();
+	const newBookInfoHandler = (e) => {
+		e.preventDefault();
 
-    fetch("https://bookworm-backend.vercel.app/books/addNewBook", {
-      method: "POST",
-      body: JSON.stringify({ ...newBookInfo }),
-      headers: { "Content-type": "application/json; charset=UTF-8" },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        newBookRef.current.innerText = data.message;
-        setTimeout(() => {
-          newBookRef.current.innerText = "";
-        }, 2000);
-      })
-      .catch((err) => console.error(err));
+		fetch("https://bookworm-backend.vercel.app/books/addNewBook", {
+			method: "POST",
+			body: JSON.stringify({ ...newBookInfo }),
+			headers: { "Content-type": "application/json; charset=UTF-8" },
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				newBookRef.current.innerText = data.message;
+				setTimeout(() => {
+					newBookRef.current.innerText = "";
+				}, 2000);
+			})
+			.catch((err) => console.error(err));
 
-    //   Reset the input fields
-    setNewBookInfo({
-      bookName: "",
-      authorName: "",
-      bookImg: "",
-      bookCategory: "",
-      bookPrice: "",
-    });
-  };
+		//   Reset the input fields
+		setNewBookInfo({
+			bookName: "",
+			authorName: "",
+			bookImg: "",
+			bookCategory: "",
+			bookPrice: "",
+		});
+	};
 
-  const value = {
-    section,
-    setSection,
-    sidebarRef,
-    newBookRef,
-    newBookInfo,
-    setNewBookInfo,
-    showSidebar,
-    newBookInfoHandler,
-  };
+	const value = {
+		section,
+		setSection,
+		sidebarRef,
+		newBookRef,
+		newBookInfo,
+		setNewBookInfo,
+		showSidebar,
+		newBookInfoHandler,
+	};
 
-  return (
-    <DashboardContext.Provider value={value}>
-      {children}
-    </DashboardContext.Provider>
-  );
+	return (
+		<DashboardContext.Provider value={value}>
+			{children}
+		</DashboardContext.Provider>
+	);
 };
 
 export { DashboardContext, DashboardProvider };
